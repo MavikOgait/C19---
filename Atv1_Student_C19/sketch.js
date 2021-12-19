d1d3293a99610efa5253d5e52f05e5c2f60f0719
Maria Vitória Tiago Inocêncio <94401852+MavikOgait@users.noreply.github.com>
2021-12-19 20:44:37 -0300

    Add files via upload

---
+++ Atv1_Student_C19/sketch.js
@@ -0,0 +1,24 @@
+var towerImg, tower;
+
+
+function preload(){
+  towerImg = loadImage("tower.png");
+  
+}
+
+function setup() {
+  createCanvas(600, 600);
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
+  
+}
+
+function draw() {
+  background(200);
+  
+  if(tower.y > 400){
+      tower.y = 300
+    }
+  drawSprites();
+}
